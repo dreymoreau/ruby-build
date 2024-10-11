26ceaf04f931d224c80db79a047c09e888d2f8c3
dreymotoh <striclyspoken@gmail.com>
2024-10-11 11:13:18 -0400

    rearrage

--- codewars/credit_card_mask.rb
+++ codewars/credit_card_mask.rb
@@ -2,12 +2,14 @@
 # first check if the length of the cc parameter is 4 elements or less,, return it right away
 # declare str variable to cc and use range method to get all elements except the last 4
 # declare nums and do the same thing but get the last 4 elements using slice operator
-
-# key differences!!
-# ..	Creates a range that includes the end index.	Yes	str[0..4] gives "Hello".
-# ,	Takes a starting index and a length.	No	str[0, 5] gives "Hello".
 # declare masked_part variable and convert str into an array using chars then map through converting everything to "#" join back
 # return masked_part + nums,, as one string
+
+
+# key differences!! .. ((range operator)) and , ((slice operator))
+# ..	Creates a range that includes the end index.	Yes	str[0..4] gives "Hello".
+# ,	Takes a starting index and a length.	No	str[0, 5] gives "Hello".
+
 
 def maskify(cc)
   return cc if cc.length <= 4
